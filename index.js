5b7b21a4d7c3d34bd52a926375389090582f5e19
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-02-15 11:43:20 -0500

    -

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
+require('dotenv').config();
 
-dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL, () => {
     console.log('Connected to MongoDB');
@@ -23,6 +22,6 @@ app.use("/api/users" , userRoute)
 app.use("/api/auth" , authRoute)
 app.use("/api/posts" , postRoute)
 
-app.listen(8000, () => {
+app.listen(8001, () => {
     console.log('Backened server is running!');
 })
